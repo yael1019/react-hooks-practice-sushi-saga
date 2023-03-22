ba708f232f7cc23fb62a8d8b75956d764d349938
Yael Harosh <yaelharosh01@gmail.com>
2023-03-22 12:37:49 -0400

    finish delivs

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 
 function Sushi({ sush, setEmptyPlates, emptyPlates, budget, setBudget }) {
-// INITIALIZE STATE FOR EATING SUSHI
+  // INITIALIZE STATE FOR EATING SUSHI
   const [eatSush, setEatSush] = useState(false);
-// FUNCTION TO TOGGLE EATSUSH
+  // FUNCTION TO TOGGLE EATSUSH
   function handleClick() {
-    setEatSush(eatSush => !eatSush);
-    setEmptyPlates([...emptyPlates, sush])
-    setBudget(budget - sush.price)
+    if (budget > sush.price) {
+      setEatSush(eatSush => !eatSush);
+      setEmptyPlates([...emptyPlates, sush])
+      setBudget(budget - sush.price)
+    }
   }
   return (
     <div className="sushi">
-      <div className="plate" onClick={ handleClick }>
+      <div className="plate" onClick={handleClick}>
         {/* Tell me if this sushi has been eaten! */}
         {eatSush ? null : (
           <img
-            src={ sush.img_url }
-            alt={ sush.name }
+            src={sush.img_url}
+            alt={sush.name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        { sush.name } - ${ sush.price }
+        {sush.name} - ${sush.price}
       </h4>
     </div>
   );
